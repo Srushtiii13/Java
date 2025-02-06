1f7586505491936134c0be4b8ee6047e58edd56e
Srushtiii13 <139744794+Srushtiii13@users.noreply.github.com>
2025-02-07 01:13:32 +0530

    Add files via upload

---
+++ AreaAndPerimeterOfRectangle.java
@@ -0,0 +1,12 @@
+class AreaAndPerimeterOfRectangle 
+{
+	public static void main(String[] args) 
+	{
+		float width = 4.5f ;
+		float height = 7.9f ;
+		float area = width * height;
+		float perimeter = 2*(width + height);
+		System.out.println("Area of rectangle of width 4.5 and height 7.9 is : " + area);
+		System.out.println("Perimeter of rectangle of width 4.5 and height 7.9 is : " + perimeter);
+	}
+}
